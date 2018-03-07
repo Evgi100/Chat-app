6f97fc4319f4a0a5ba2500e4ad180083202262fc
Evgi100 <evgeni.shapira@gmail.com>
2018-03-07 15:29:04 +0200

    Finished main functionality and add basic design

---
+++ public/js/chat.js
@@ -0,0 +1,131 @@
+var socket = io();
+
+function scrollToBottom() {
+    // Selectors
+    var messages = $('#messages');
+    var newMessage = messages.children('li:last-child')
+    //Heights
+    var clientHeight = messages.prop('clientHeight');
+    var scrollTop = messages.prop('scrollTop');
+    var scrollHeight = messages.prop('scrollHeight');
+    var newMessageHeight = newMessage.innerHeight();
+    var lastMessageHeight = newMessage.prev().innerHeight();
+
+    if (clientHeight + scrollTop + lastMessageHeight + newMessageHeight >= scrollHeight) {
+        messages.scrollTop(scrollHeight);
+    }
+}
+
+socket.on('connect', () => {
+    var params = $.deparam(window.location.search);
+    socket.emit('join', params, function (err) {
+        if (err) {
+            alert(err)
+            window.location.href = "/";
+        } else {
+            console.log('No error');
+        }
+    })
+})
+
+socket.on('disconnect', () => {
+    
+});
+
+socket.on('updateUserList', function (users) {
+    var ol = $('<ol></ol>');
+    users.forEach(function (user) {
+        ol.append($('<li></li>').text(user))
+    });
+
+    $('#users').html(ol);
+});
+
+socket.on('newMessage', function (message) {
+    var formattedTime = moment(message.createdAt).format('h:mm a');
+    var template = $('#message-template').html();
+    var html = Mustache.render(template, {
+        text: message.text,
+        from: message.from,
+        createdAt: formattedTime
+    });
+
+    $('#messages').append(html);
+    scrollToBottom();
+});
+
+socket.on('welcomeMessage', function (message) {
+    var formattedTime = moment(message.createdAt).format('h:mm a');
+    var template = $('#message-template').html();
+    var html = Mustache.render(template, {
+        text: message.text,
+        from: message.from,
+        createdAt: formattedTime
+    });
+
+    $('#messages').append(html);
+    scrollToBottom();
+});
+
+socket.on('newUser', function (message) {
+    var formattedTime = moment(message.createdAt).format('h:mm a');
+    var template = $('#message-template').html();
+    var html = Mustache.render(template, {
+        text: message.text,
+        from: message.from,
+        createdAt: formattedTime
+    });
+    $('#messages').append(html);
+    scrollToBottom();
+});
+
+socket.on('newLocationMessage', function (message) {
+    var formattedTime = moment(message.createdAt).format('h:mm a');
+    var template = $('#location-message-template').html();
+    var html = Mustache.render(template, {
+        url: message.url,
+        from: message.from,
+        createdAt: formattedTime
+    });
+    $('#messages').append(html);
+    scrolToBottom();
+
+})
+
+
+$('#message-form').on('submit', function (e) {
+    e.preventDefault();
+
+    socket.emit('createMessage', {
+        text: $('[name=message]').val()
+    }, function () {
+        $('[name=message]').val('')
+    }
+    )
+});
+
+
+var locationBtn = $('#send-location');
+
+locationBtn.on('click', function () {
+    if (!navigator.geolocation) {
+        return alert('Geolocation not supported by your browser')
+    }
+
+    locationBtn.attr('disabled', 'disabled').text('Sending location')
+
+    navigator.geolocation.getCurrentPosition(function (position) {
+        locationBtn.removeAttr('disabled').text('Send location')
+        socket.emit('createLocationMessage', {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+        });
+    }, function () {
+        locationBtn.removeAttr('disabled')
+        alert('Unable to fetch location')
+    });
+});
+
+
+
+
